9fa6e5fc0bc15bc3625c32df584841c8eacae4ed
WB-Jang <eeariorie@gmail.com>
2025-11-24 17:49:26 +0900

    Enhance keywords extractor with error handling
    Refactor keywords_extractor function to handle exceptions and print debug information.

--- app/llm_keyword_extractor.py
+++ app/llm_keyword_extractor.py
@@ -2,6 +2,13 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+from typing import literal, Dict, Ant
+from pydantic import BaseModel
+from functools import lru_cache
+
+import json as json
+import ast
+
 # llama-server.exe -m "path/model.gguf" --top-p 0.9 --repeat-penalty 1.15 --host 127.0.0.1 --port 8080
 BASE_URL = "http://127.0.0.1:8080/v1" #generation mode
 
@@ -15,18 +22,23 @@ llm = ChatOpenAI(
 prompt = ChatPromptTemplate.from_messages([
   ("system","""
   You are AI assistant to extract keywords which are helpful to understand user`s query.
-  While extracting the keywords, AI assistant should consider what keyword might be essential to intention of [질문] in the form of JSON
+  While extracting the keywords, AI assistant should consider what keyword might be essential to intention of [질문] in the form of example
 
   <example>
-   json
-   {
-   'keywords' : ['keyword1','keyword2','keyword3',...]
-   }),
+   ['keyword1','keyword2','keyword3',...]
+   """),
   ("human", "[질문]\n{user_query}\n")
 ])
 
 chain = prompt | llm | StrOutputParser()
 
 def keywords_extractor(query):
-  generation = chain.invoke({"user_query": query})
+  print('extractor 실행')
+  try:
+    generation = chain.invoke({"user_query": state.user_input})
+    l = ast.literal_eval(generation)
+    state.keywords = l
+  except Exception:
+    print('extractor failure')
+  return {"keywords": l}
   print(generation)
