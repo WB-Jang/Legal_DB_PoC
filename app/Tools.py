44d97614540865cfb33358da48dc1157a46507d4
WB-Jang <eeariorie@gmail.com>
2025-11-24 17:44:39 +0900

    Update rerank_by_api to accept AgentState
    Refactor rerank_by_api to use AgentState and search keywords.

--- app/Tools.py
+++ app/Tools.py
@@ -1,4 +1,5 @@
-from app._faiss import faiss_vector_db
+from _faiss import faiss_vector_db
+from llm_keyword_extractor import AgentState
 import requests
 
 db = faiss_vector_db(
@@ -7,22 +8,22 @@ db = faiss_vector_db(
     save_nm="privacy_store.json"
 )
 
-def rerank_by_api(query):
-    search_results = db._search_db(query, k=30)
-    candidate_texts = [r[0] for r in search_results]
+def rerank_by_api(state: AgentState):
+    candidate_texts = []
+    for key in state.keywords:
+        search_results = db._search_db(key, k=3)
+        candidate_texts.append(search_results)
+    print('1st ranking result : ',candidate_texts)
+    
     api_url = "http://127.0.0.1:8082/v1/rerank"
     
     payload = {
-        "query": query,
+        "query": state.user_input,
         "candidates": candidate_texts
     }
     r = requests.post(api_url, json=payload, timeout=30)
     results = r.json()["results"]  # [{"text": ..., "score": ...}, ...]
-    # 점수 내림차순 정렬
+   
     return sorted(results, key=lambda x: x["score"], reverse=True)
 
-query = "은행의 리스크 관리가 뭔가요?"
-reranked_results = rerank_by_api(query)
 
-for res in reranked_results:
-    print(f"문장: {res['text']}\n서버Rerank 점수: {res['score']}")
